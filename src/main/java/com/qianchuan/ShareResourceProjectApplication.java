918533658325f1cc469a69416444a5397d5bb183
chuanqian <1178995389@qq.com>
2022-06-30 12:48:52 +0800

    创建一个分享资源的后端项目share-resources-project

---
+++ src/main/java/com/qianchuan/ShareResourceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qianchuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareResourceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareResourceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qianchuan/ShareResourceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qianchuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareResourceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
